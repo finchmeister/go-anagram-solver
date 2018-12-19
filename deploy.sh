a82f84177ca0378792ceb199e701bdd4674b302b
Jonathan Finch <thefinchmeister@gmail.com>
2018-12-19 22:00:09 +0000

    add travis

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-git fetch -a
-git checkout -f origin/master
-go get -d ./...
-go build main.go
-sudo service goanagram restart
--- install.sh
+++ install.sh
@@ -3,3 +3,20 @@
 sudo apt-get install golang-go
 sudo apt-get install wbritish
 
+chmod +x update.sh
+chmod +x install.sh
+
+echo "[Unit]
+Description=goanagram
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=5s
+ExecStart=/home/thefinchmeister/go-anagram-solver/main
+WorkingDirectory=/home/thefinchmeister/go-anagram-solver
+
+[Install]
+WantedBy=multi-user.target" | sudo tee --append /lib/systemd/system/goanagram.service > /dev/null
+
+sudo service goanagram start
