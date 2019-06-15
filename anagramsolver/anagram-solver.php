85bdc60ff4970054a5256df691d86f0218cf518f
Jonathan Finch <thefinchmeister@gmail.com>
2019-06-15 19:30:09 +0100

    php hacky anagram solver

---
+++ anagramsolver/anagram-solver.php
@@ -0,0 +1,185 @@
+<?php
+
+class AnagramSolver
+{
+    /**
+     * @var int
+     */
+    private $minWordLength;
+    /** @var array */
+    private $dictionary;
+    private $lettersCount;
+    private $anagrams;
+
+    public function __construct(
+        int $minWordLength
+    ) {
+        $this->minWordLength = $minWordLength;
+        $this->loadDictionary();
+    }
+
+    private function loadDictionary()
+    {
+        $this->dictionary = explode(PHP_EOL, file_get_contents(__DIR__.'/dictionary.txt'));
+    }
+
+    public function nextPermutation(&$input)
+    {
+        $inputCount = count($input);
+        // the head of the suffix
+        $i = $inputCount - 1;
+        // find longest suffix
+        while ($i > 0 && $input[$i] <= $input[$i - 1]) {
+            $i--;
+        }
+        //are we at the last permutation already?
+        if ($i <= 0) {
+            return false;
+        }
+        // get the pivot
+        $pivotIndex = $i - 1;
+        // find rightmost element that exceeds the pivot
+        $j = $inputCount - 1;
+        while ($input[$j] <= $input[$pivotIndex]) {
+            $j--;
+        }
+
+        // swap the pivot with j
+        $temp = $input[$pivotIndex];
+        $input[$pivotIndex] = $input[$j];
+        $input[$j] = $temp;
+        // reverse the suffix
+        $j = $inputCount - 1;
+        while ($i < $j) {
+            $temp = $input[$i];
+            $input[$i] = $input[$j];
+            $input[$j] = $temp;
+            $i++;
+            $j--;
+        }
+        return true;
+    }
+
+    public function anagramSolver(string $letters)
+    {
+        $letters = str_split($letters);
+        sort($letters);
+        $this->lettersCount = count($letters);
+
+        $this->anagrams = [];
+
+        foreach ($this->dictionary as $word) {
+//            $this->compareWord($word, $letters);
+            a:
+            $wordLength = strlen($word);
+            for ($i = 0; $i < $wordLength; $i++) {
+                if ($wordLength < $this->minWordLength || $wordLength > $this->lettersCount) {
+                    // next word (word is too short or long)
+                    continue 2;
+                }
+
+                if ($word[$i] < $letters[$i]) {
+                    // next word (word is ordered before)
+                    continue 2;
+                }
+
+                if ($word[$i] === $letters[$i]) {
+                    if ($i === $wordLength - 1) {
+                        $this->anagrams[] = $word;
+                        // next word (we've found an anagram)
+                        continue 2;
+                    }
+                    continue;
+                }
+                // keep permutating word until it is greater than dictionary
+                $lettersBefore = $letters;
+                $this->nextPermutation($letters);
+                if ($letters === $lettersBefore) {
+                    break;
+                }
+                goto a;
+            }
+        }
+
+        return $this->anagrams;
+    }
+
+    public function compareWord(string $word, array $letters)
+    {
+        $wordLength = strlen($word);
+        for ($i = 0; $i < $wordLength; $i++) {
+            if ($wordLength < $this->minWordLength || $wordLength > $this->lettersCount) {
+                // next word (word is too short or long)
+                return;
+            }
+
+            if ($word[$i] < $letters[$i]) {
+                // next word (word is ordered before)
+                return;
+            }
+
+            if ($word[$i] === $letters[$i]) {
+                if ($i === $wordLength - 1) {
+                    $this->anagrams[] = $word;
+                    // next word (we've found an anagram)
+                    return;
+                }
+                continue;
+            }
+            // keep permutating word until it is greater than dictionary
+            $lettersBefore = $letters;
+            $this->nextPermutation($letters);
+            if ($letters === $lettersBefore) {
+                print_r($this->anagrams);
+                exit;
+            }
+            $this->compareWord($word, $letters);
+        }
+    }
+}
+
+
+
+
+$expected = array (
+    0 => 'aardvark',
+    1 => 'ada',
+    2 => 'adar',
+    3 => 'aka',
+    4 => 'aka',
+    5 => 'akra',
+    6 => 'akra',
+    7 => 'ara',
+    8 => 'arad',
+    9 => 'arar',
+    10 => 'arara',
+    11 => 'arara',
+    12 => 'ark',
+    13 => 'ava',
+    14 => 'avar',
+    15 => 'dak',
+    16 => 'dar',
+    17 => 'dark',
+    18 => 'darr',
+    19 => 'kava',
+    20 => 'kra',
+    21 => 'kra',
+    22 => 'raad',
+    23 => 'rad',
+    24 => 'rada',
+    25 => 'radar',
+    26 => 'vara',
+    27 => 'varda',
+);
+
+$a = new AnagramSolver(3);
+$start = microtime(true);
+$result = $a->anagramSolver('aardvark');
+$timeTaken = microtime(true) - $start;
+
+if ($result === $expected) {
+    echo 'SUCCESS: '.$timeTaken;
+} else {
+    echo 'FAIL';
+    print_r($result);
+}
